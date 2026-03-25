331e1171d6267fdd63797808db36ca10103434dd
Josh Lord <joshlord21@gmail.com>
2026-03-25 19:16:59 -0400

    update

--- pokerengine.py
+++ pokerengine.py
@@ -15,7 +15,9 @@ class Card:
 class Deck:
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-    rank_orders = {rank: index for index, rank in enumerate(ranks, start=2)}
+    rank_orders = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+                   "7": 7, "8": 8, "9": 9, "10": 10,
+                   "Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
 
     def __init__(self):
         self.cards = [Card(suit, rank) for suit in self.suits for rank in self.ranks]
